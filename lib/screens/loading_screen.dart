3820c27d926dca2da884590be519f55603c9fcf7
Hgamo <73846452+Hgamo@users.noreply.github.com>
2020-11-21 22:39:41 +0100

    new loading screen

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,14 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-class LoadingScreen extends StatelessWidget {
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
   @override
   Widget build(BuildContext context) {
     bool isdark = MediaQuery.of(context).platformBrightness == Brightness.dark;
@@ -10,8 +18,8 @@ class LoadingScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Padding(
-              padding: const EdgeInsets.all(20.0),
+            LoadnigAnimation(
+              maxwidth: MediaQuery.of(context).size.width - 40,
               child: Image.asset(
                   isdark ? 'assets/logo_dunkel.png' : 'assets/logo_hell.png'),
             ),
@@ -20,5 +28,84 @@ class LoadingScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class LoadnigAnimation extends StatefulWidget {
+  final Widget child;
+  final double maxwidth;
+  LoadnigAnimation({this.child, this.maxwidth});
+  @override
+  _LoadnigAnimationState createState() => _LoadnigAnimationState();
+}
+
+class _LoadnigAnimationState extends State<LoadnigAnimation> {
+  double width = 0;
+  double height = 0;
+  Color color = Colors.blue;
+  double radius = 5;
+  void changewidth() async {
+    setState(() {
+      if (width == widget.maxwidth / 4) {
+        width = widget.maxwidth;
+      } else {
+        width = widget.maxwidth / 4;
+      }
+    });
+    await Future.delayed(Duration(seconds: 2));
+    changewidth();
+  }
+
+  void someRandom() async {
+    if (!this.mounted) {
+      return;
+    }
+    setState(() {
+      color = UniqueColorGenerator.getColor();
+      width =
+          Random().nextInt((widget.maxwidth - 100).toInt()).toDouble() + 100;
+      height = Random().nextInt(100).toDouble() + 100;
+      radius = Random().nextInt(50).toDouble();
+    });
+    await Future.delayed(Duration(seconds: 2));
+    someRandom();
+  }
+
+  @override
+  void initState() {
+    someRandom();
+    //changewidth();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      decoration: BoxDecoration(
+        color: Theme.of(context).accentColor,
+        borderRadius: BorderRadius.all(
+          Radius.circular(radius),
+        ),
+      ),
+      child: widget.child,
+      curve: Curves.easeInOutBack,
+      width: width,
+      height: height,
+      duration: Duration(seconds: 2),
+    );
+    // return AnimatedContainer(
+    //   curve: Curves.easeInOut,
+    //   width: width,
+    //   child: widget.child,
+    //   duration: Duration(seconds: 2),
+    // );
+  }
+}
+
+class UniqueColorGenerator {
+  static Random random = new Random();
+  static Color getColor() {
+    return Color.fromARGB(
+        255, random.nextInt(255), random.nextInt(255), random.nextInt(255));
   }
 }
